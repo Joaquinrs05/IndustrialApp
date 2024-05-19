f0320a5f1e9dbc3afb75c63edb8777e8101d9f3a
Joaquín Rodríguez <117047056+Joaquinrs05@users.noreply.github.com>
2024-05-19 19:04:11 +0200

    Add files via upload

---
+++ app/src/main/java/com/politecnicomalaga/maquinaindustrial/informacion.java
@@ -0,0 +1,18 @@
+package com.politecnicomalaga.maquinaindustrial;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class informacion extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.informacion); // Asegúrate de tener informacion.xml en res/layout
+    }
+}
+
