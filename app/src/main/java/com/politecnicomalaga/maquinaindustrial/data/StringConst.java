d8576682c15999a79b7c498e6d81ec331bb2c965
Joaquín Rodríguez <117047056+Joaquinrs05@users.noreply.github.com>
2024-05-29 08:20:10 +0200

    Cambios Preguntas

--- app/src/main/java/com/politecnicomalaga/maquinaindustrial/data/StringConst.java
+++ app/src/main/java/com/politecnicomalaga/maquinaindustrial/data/StringConst.java
@@ -3,10 +3,10 @@ package com.politecnicomalaga.maquinaindustrial.data;
 public class StringConst {
     public static String pregunta1 = "¿En qué año se construyó la fundación del plomo 'Los Guindos'?";
 
-    public static String respuesta1A = "Cuando Joaquin empezó a andar";
+    public static String respuesta1A = "1934";
     public static String respuesta1B = "1923";
-    public static String respuesta1C = "Cuando Chema se rompio el tobillo";
-    public static String respuesta1D = "Si";
+    public static String respuesta1C = "2024";
+    public static String respuesta1D = "1987";
 
     public static String pregunta2 = "¿Qué protección tiene la fabrica de electricidad FiatLux?";
     public static String respuesta2A = "BIC";
@@ -16,12 +16,12 @@ public class StringConst {
 
     public static String pregunta3 = "¿Qué catalogación tiene la fábrica de la tabacalera?";
 
-    public static String respuesta3A = "Politécnico";
-    public static String respuesta3B = "Jesús";
+    public static String respuesta3A = "Arquitectura Barroca";
+    public static String respuesta3B = "Arquitectura Tradicional";
     public static String respuesta3C = "Arquitectura industrial";
-    public static String respuesta3D = "Marín";
+    public static String respuesta3D = "Arquitectura simple";
 
-    public static String pregunta4 = "¿Cuándo se construyó Pisos Picados?";
+    public static String pregunta4 = "¿Cuándo se construyó la fabrica de Santa Inés?";
 
     public static String respuesta4A = "2018";
     public static String respuesta4B = "1820";
